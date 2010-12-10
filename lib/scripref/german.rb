84d62a38d2014f7689b590d17e2f38a8c8d9e18c
janfri <janfri26@gmail.com>
2010-12-10 21:07:25 +0100

    German booknames completed; encoding directive inserted.

--- lib/scripref/german.rb
+++ lib/scripref/german.rb
@@ -2,8 +2,16 @@
 module Scripref::German
 
   def book_names
-    @booknames ||= %q(1. Mose, 2.Mose, 3. Mose, 4. Mose, 5. Mose, Josua, Richter, Ruth, 1. Samuel 2. Samuel
-        1. Könige, 2.Könige, 1. Chronika, 2. Chronika, Esra, Nehemia).split(/,\s*/)
+    @booknames ||= <<-END.split(/,\s*/)
+      1. Mose, 2.Mose, 3. Mose, 4. Mose, 5. Mose, Josua, Richter, Ruth, 1. Samuel, 2. Samuel,
+      1. Könige, 2.Könige, 1. Chronika, 2. Chronika, Esra, Nehemia, Esther, Hiob, Psalm,
+      Sprüche, Prediger, Hohelied, Jesaja, Jeremia, Klagelieder, Hesekiel, Daniel, Hosea, Joel,
+      Amos, Obadja, Jona, Micha, Nahum, Habakuk, Zephanja, Haggai, Sacharja, Maleachi,
+      Matthäus, Markus, Lukas, Johannes, Apostelgeschichte, Römer, 1. Korinther, 2. Korinther,
+      Galater, Epheser, Philipper, Kolosser, 1. Thessalonicher, 2. Thessalonicher, 1. Timotheus,
+      2. Timotheus, Titus, Philemon, Hebräer, Jakobus, 1. Petrus, 2. Petrus, 1. Johannes,
+      2. Johannes, 3. Johannes, Judas, Offenbarung
+    END
   end
 
   def book_re
