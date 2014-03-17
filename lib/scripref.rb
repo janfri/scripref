c2ecd4c31bae5810d58ceada44328cf3ebc2b79d
Jan Friedrich <janfri26@gmail.com>
2014-03-17 21:45:50 +0100

    Allow further options in Passage struct.

--- lib/scripref.rb
+++ lib/scripref.rb
@@ -8,9 +8,12 @@ module Scripref
 
   VERSION = '0.2.0'
 
-  Passage = Struct.new(:text, :b1, :c1, :v1, :b2, :c2, :v2)
+  Passage = Struct.new(:text, :b1, :c1, :v1, :b2, :c2, :v2, :a1, :a2) do
 
-  class Passage
+    def initialize text, b1, c1, v1, b2, c2, v2, opts={}
+      super text, b1, c1, v1, b2, c2, v2, opts[:a1], opts[:a2]
+    end
+
     alias to_s text
   end
 
