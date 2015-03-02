a29bb0e140b362cf2af3dbf8e48fcfa225b785bc
Jan Friedrich <janfri26@gmail.com>
2015-03-02 16:16:03 +0100

    Refactoring: Rename method.

--- test/test_formatter.rb
+++ test/test_formatter.rb
@@ -20,30 +20,30 @@ class TestFormatter < Test::Unit::TestCase
   def test_one_verse
     @german = 'Römer 6,23'
     @english = 'Romans 6:23'
-    assert_formatting
+    check_formatting
   end
 
   def test_simple_passage
     @german = 'Römer 8,1-10'
     @english = 'Romans 8:1-10'
-    assert_formatting
+    check_formatting
   end
 
   def test_passage_with_chapter_change
     @german = 'Römer 1,1-5,11'
     @english = 'Romans 1:1-5:11'
-    assert_formatting
+    check_formatting
   end
 
   def test_passage_with_book_change
     @german = '1. Korinther 1,1-2. Korinther 13,13'
     @english = '1 Corinthians 1:1-2 Corinthians 13:13'
-    assert_formatting
+    check_formatting
   end
 
   private
 
-  def assert_formatting
+  def check_formatting
     ast = @parser.parse(@german)
     assert_equal @german, @german_formatter.fullref(ast)
     assert_equal @english, @english_formatter.fullref(ast)
