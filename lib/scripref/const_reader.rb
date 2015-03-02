6de16287e60c41608af29bfbbcea70a67ab23e18
Jan Friedrich <janfri26@gmail.com>
2015-03-02 17:27:28 +0100

    New method: ConstReader#const_accessor.

--- lib/scripref/const_reader.rb
+++ lib/scripref/const_reader.rb
@@ -43,4 +43,40 @@ module ConstReader
     end
   end
 
+  # Define a getter and a setter method for one or more constant values.
+  # The value of the constant itself is not changed, instead
+  # a corresponding instance variable is used.
+  #
+  # Example:
+  #   module M
+  #     A = :ma
+  #     extend ConstReader
+  #     const_accessor :A
+  #   end
+  #   class C
+  #     include M
+  #   end
+  #   c1 = C.new
+  #   c1.a # => :ma
+  #   c1.a = :ca
+  #   c1.a # => :ca
+  #   c2 = C.new
+  #   c2.a # => :ma
+  def const_accessor *consts
+    consts.flatten.each do |c|
+      class_exec(c.to_s, c.to_s.downcase) do |c_name, m_name|
+        define_method m_name do
+          ivar = '@' << m_name
+          if instance_variable_defined? ivar
+            return instance_variable_get(ivar)
+          else
+            val = self.singleton_class.const_get c_name
+            instance_variable_set ivar, val
+            return val
+          end
+        end
+        attr_writer m_name
+      end
+    end
+  end
 end
