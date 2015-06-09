02d7e3ed8225aa34493bbc73f0fcb669ebf53496
Jan Friedrich <janfri26@gmail.com>
2015-06-09 20:03:51 +0200

    Fix typo in German book names.

--- lib/scripref/german.rb
+++ lib/scripref/german.rb
@@ -9,7 +9,7 @@ module Scripref
     # Array of book names.
     BOOK_NAMES = <<-END.strip.split(/,\s*/)
       1. Mose, 2. Mose, 3. Mose, 4. Mose, 5. Mose, Josua, Richter, Ruth, 1. Samuel, 2. Samuel,
-      1. Könige, 2.Könige, 1. Chronika, 2. Chronika, Esra, Nehemia, Esther, Hiob, Psalm,
+      1. Könige, 2. Könige, 1. Chronika, 2. Chronika, Esra, Nehemia, Esther, Hiob, Psalm,
       Sprüche, Prediger, Hohelied, Jesaja, Jeremia, Klagelieder, Hesekiel, Daniel, Hosea, Joel,
       Amos, Obadja, Jona, Micha, Nahum, Habakuk, Zephanja, Haggai, Sacharja, Maleachi,
       Matthäus, Markus, Lukas, Johannes, Apostelgeschichte, Römer, 1. Korinther, 2. Korinther,
