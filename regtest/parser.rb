362c7b31616dfa1a34c0e8658c89df168e54fe7f
Jan Friedrich <janfri26@gmail.com>
2015-08-06 21:52:09 +0200

    Generate invalid parser samples for regtest.

--- regtest/parser.rb
+++ regtest/parser.rb
@@ -4,13 +4,22 @@ require 'scripref'
 include Regtest
 include Scripref
 
-$parser = Parser.new(German)
-
 def s text
   sample text do
-    res = $parser.parse(text)
+    res = Parser.new(German).parse(text)
     res.map {|r| r.respond_to?(:to_h) ? r.to_h : r.to_s}
   end
 end
 
-s 'Ruth 2,1a-11.15a; 3,7b.9-12b; Markus 4; 5,3a.18b-21a'
+text = 'Ruth 2,1a-11.15a; 3,7b.9-12b; Markus 4; 5,3a.18b-21a'
+
+s text
+
+a = text.split(/\b/)
+a.each do |e|
+  next if e =~ /^\s+$/
+  a2 = a.clone
+  a2.delete(e)
+  t = a2.join
+  s t
+end
