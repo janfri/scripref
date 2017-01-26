0179f5f3002087e209eae708f67f3bed237dcc2a
Jan Friedrich <janfri26@gmail.com>
2017-01-26 21:00:01 +0100

    Refactoring tests for processor and add a new one.

--- test/test_processor.rb
+++ test/test_processor.rb
@@ -69,16 +69,27 @@ class TestProcessor < Test::Unit::TestCase
   def test_numerical_context
     text = '1. Mt 1,1'
     processor = Processor.new(text, German)
-    ast = [[pass('Mt 1,1 ', 40, 1, 1, 40, 1, 1)]]
+    ast = [[pass(text, 40, 1, 1, 40, 1, 1)]]
     assert_equal ast, processor.each_ref.to_a
-    ast = ['1. ', [pass('Mt 1,1 ', 40, 1, 1, 40, 1, 1)]]
+    ast = ['1. ', [pass(text, 40, 1, 1, 40, 1, 1)]]
     assert_equal ast, processor.each.to_a
   end
 
   def test_verse_addon
     text = 'Mt 1,1a'
     processor = Processor.new(text, German)
-    ast = [[pass('Mt 1,1a', 40, 1, 1, 40, 1, 1, a1: 'a')]]
+    ast = [[pass(text, 40, 1, 1, 40, 1, 1, a1: 'a')]]
+    assert_equal ast, processor.each.to_a
+  end
+
+  def test_verse_postfixes
+    text = 'Mt 1,1f'
+    processor = Processor.new(text, German)
+    ast = [[pass(text, 40, 1, 1, 40, 1, :f)]]
+    assert_equal ast, processor.each.to_a
+    text = 'Mt 1,1ff'
+    processor = Processor.new(text, German)
+    ast = [[pass(text, 40, 1, 1, 40, 1, :ff)]]
     assert_equal ast, processor.each.to_a
   end
 
