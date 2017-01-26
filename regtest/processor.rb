882c1e5db395a3a84473467302aba63b63d30ec2
Jan Friedrich <janfri26@gmail.com>
2017-01-26 20:45:30 +0100

    Save status quo with regtest for processor.

---
+++ regtest/processor.rb
@@ -0,0 +1,25 @@
+require 'regtest'
+require 'scripref'
+
+include Regtest
+include Scripref
+
+def s text
+  sample text do
+    p = Processor.new(text, German)
+    p.each.to_a
+  end
+end
+
+text = 'Ruth 2,1a-11.15a; 3,7b.9-12b; Markus 4; 5,3a.18b-21a'
+
+s text
+
+a = text.split(/\b/)
+a.each_with_index do |e, i|
+  next if e =~ /^\s+$/
+  a2 = a.clone
+  a2.delete_at(i)
+  t = a2.join
+  s t
+end
