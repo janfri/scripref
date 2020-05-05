5fc400f6d52fa416afa135ff2c82cc810e772aa3
Jan Friedrich <janfri26@gmail.com>
2020-05-05 16:19:57 +0200

    Make instances of Passage comparable again

--- lib/scripref/passage.rb
+++ lib/scripref/passage.rb
@@ -11,48 +11,15 @@ module Scripref
 
     def <=> other
       return unless other.kind_of? Passage
-      self.make_comparable.to_a <=> other.make_comparable.to_a
-    end
-
-    # Returns a copy which is comparable, that means
-    # all values are numeric.
-    # This is a heuristic approach.
-    def make_comparable max: 999, ff: 3
-      self.dup.make_comparable! max: max, ff: ff
-    end
-
-    # Makes the Passage instance comparable, that means
-    # all values are numeric.
-    # This is a heuristic approach.
-    def make_comparable! max: 999, ff: 3
-      self.b1 ||= 1
-      self.c1 ||= 1
-      self.v1 ||= 1
-      self.b2 ||= max
-      self.c2 ||= max
-      self.v2 ||= max
-      if self.v2 == :f
-        self.v2 = self.v1 + 1
-      end
-      if self.v2 == :ff
-        self.v2 = self.v1 + ff
-      end
-      self
-    end
-
-    # Checks if the instance is comparable, that means
-    # all values are numeric and so the <=> method
-    # can be applied.
-    def comparable?
-      to_a.map {|e| Numeric === e}.uniq == [true]
+      Passage.make_comparable(self) <=> Passage.make_comparable(other)
     end
 
     # Returns true if the instance and the given passage overlap.
     # That means both has at least one verse in common.
     def overlap? passage
       fail ArgumentError, 'value must be a passage' unless passage.kind_of? Passage
-      a = self.make_comparable.to_a
-      b = passage.make_comparable.to_a
+      a = Passage.make_comparable(self)
+      b = Passage.make_comparable(passage)
       [a[0..2] <=> b[3..5], b[0..2] <=> a[3..5]].max < 1
     end
 
@@ -67,6 +34,42 @@ module Scripref
     end
 
     alias to_s text
+
+    @book_id2num = {}
+    osis_book_ids = %i[
+      Gen Exod Lev Num Deut Josh Judg Ruth 1Sam 2Sam 1Kgs 2Kgs 1Chr 2Chr Ezra
+      Neh Esth Job Ps Prov Eccl Song Isa Jer Lam Ezek Dan Hos Joel Amos Obad
+      Jonah Mic Nah Hab Zeph Hag Zech Mal Matt Mark Luke John Acts Rom 1Cor
+      2Cor Gal Eph Phil Col 1Thess 2Thess 1Tim 2Tim Titus Phlm Heb Jas 1Pet
+      2Pet 1John 2John 3John Jude Rev
+    ]
+    osis_book_ids.each_with_index do |book_id, i|
+      @book_id2num[book_id] = i+1
+    end
+
+    class << self
+
+      # Makes the Passage instance comparable, that means
+      # all values are numeric.
+      # This is a heuristic approach.
+      def make_comparable pass, max: 999, ff: 3
+        b1 = @book_id2num[pass.b1]
+        c1 = pass.c1 || 1
+        v1 = pass.v1 || 1
+        b2 = @book_id2num[pass.b2]
+        c2 = pass.c2 || max
+        v2 = pass.v2 || max
+        if v2 == :f
+          v2 = v1 + 1
+        end
+        if v2 == :ff
+          v2 = v1 + ff
+        end
+        [b1, c1, v1, b2, c2, v2]
+      end
+
+    end
+
   end
 
 end
